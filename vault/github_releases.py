4119679b3ef7b36e3d5b6f0140c0efdf40ea3fbd
CultureLinux <cyklodev.services@gmail.com>
2024-11-15 16:56:31 +0100

    cleanage

--- vault/github_releases.py
+++ vault/github_releases.py
@@ -2,38 +2,24 @@ import requests
 
 class GitHubReleases:
     def __init__(self, owner, repo, token=None):
-        """
-        Initialisation de la classe avec les informations du dépôt GitHub.
-
-        :param owner: Le propriétaire du dépôt (nom d'utilisateur ou organisation)
-        :param repo: Le nom du dépôt GitHub
-        :param token: Un token GitHub pour l'authentification (facultatif)
-        """
         self.owner = owner
         self.repo = repo
         self.token = token
         self.base_url = f"https://api.github.com/repos/{owner}/{repo}/tags"
 
     def _get_headers(self):
-        """Retourne les en-têtes de la requête avec l'authentification si nécessaire."""
         headers = {}
         if self.token:
             headers["Authorization"] = f"token {self.token}"
         return headers
 
     def get_all_releases(self):
-        """
-        Récupère toutes les releases du dépôt, avec gestion de la pagination.
-
-        :return: Liste de toutes les releases
-        """
         releases = []
         url = self.base_url
 
         while url:
             response = requests.get(url, headers=self._get_headers())
             
-            # Vérifier si la requête a réussi
             if response.status_code == 200:
                 releases_data = response.json()
                 releases.extend(releases_data)
@@ -50,13 +36,8 @@ class GitHubReleases:
         return releases
 
     def display_releases(self):
-        """
-        Affiche les releases récupérées du dépôt.
-
-        :return: Rien, mais affiche les informations sur les releases dans la console
-        """
         releases = self.get_all_releases()
-
+        print("-" * 50)
         print(f"Nombre total de releases récupérées : {len(releases)}")
         for release in releases:
             print(f"Version : {release['name']}")
